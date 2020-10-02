8c3268a7ca7d9c6df75f72d522a28353fe9fd591
nelsontky <nelson@ccb.wtf>
2020-10-02 16:53:09 +0800

    Imporve audio player component

--- client/src/components/AudioPlayer.js
+++ client/src/components/AudioPlayer.js
@@ -1,20 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Slider from "@material-ui/core/Slider";
 
+// Material UI components
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import Slider from "@material-ui/core/Slider";
+import Typography from "@material-ui/core/Typography";
+
+// Icons
+import Forward10Icon from "@material-ui/icons/Forward10";
+import PauseIcon from "@material-ui/icons/Pause";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import Replay10Icon from "@material-ui/icons/Replay10";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+
+// Utils
 import { formatSeconds } from "../utils/general-utils";
 
 const VOLUME_STEP = 0.1;
+const SKIP_STEP = 10;
 
 const useStyles = makeStyles({
   controls: {
     display: "none",
+  },
+  center: {
+    textAlign: "center",
+    margin: "auto 0",
   },
 });
 
 export default function AudioPlayer() {
   const classes = useStyles();
   const [audio, setAudio] = useState(null);
+  const [duration, setDuration] = useState(0);
   const [progress, setProgress] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
 
@@ -24,11 +44,14 @@ export default function AudioPlayer() {
       const audio = document.getElementById("audio");
       const audioControls = document.getElementById("audio-controls");
 
-      // audio.controls = false;
+      audio.controls = false;
       audioControls.style.display = "block";
 
       setAudio(audio);
 
+      audio.addEventListener("loadedmetadata", () => {
+        setDuration(audio.duration);
+      });
       audio.addEventListener("timeupdate", () => {
         setCurrentTime(audio.currentTime);
         setProgress(Math.floor((audio.currentTime / audio.duration) * 100));
@@ -73,9 +96,32 @@ export default function AudioPlayer() {
     audio.currentTime = (audio.duration / 100) * value;
   };
 
+  const skip = (direction) => {
+    if (direction === "+") {
+      if (audio.currentTime + SKIP_STEP > audio.duration) {
+        audio.currentTime = audio.duration;
+      } else {
+        audio.currentTime += SKIP_STEP;
+      }
+    } else if (direction === "-") {
+      if (audio.currentTime - SKIP_STEP < 0) {
+        audio.currentTime = 0;
+      } else {
+        audio.currentTime -= SKIP_STEP;
+      }
+    }
+  };
+
+  const skipForward = () => {
+    skip("+");
+  };
+
+  const skipBackwards = () => {
+    skip("-");
+  };
+
   return (
     <figure id="audio-container">
-      <figcaption>Listen to the T-Rex:</figcaption>
       <audio
         preload="metadata"
         id="audio"
@@ -85,28 +131,52 @@ export default function AudioPlayer() {
         Your browser does not support the
         <code>audio</code> element.
       </audio>
-
-      <ul id="audio-controls" className={classes.controls}>
-        <li>
-          <button onClick={playPause} id="play-pause" type="button">
-            Play/Pause
-          </button>
-        </li>
-        <li>
-          <Slider value={progress} onChange={changeProgress} />
-        </li>
-        <li>{formatSeconds(currentTime)}</li>
-        <li>
-          <button onClick={volIncrease} id="vol-inc" type="button">
-            Vol+
-          </button>
-        </li>
-        <li>
-          <button onClick={volDecrease} id="vol-dec" type="button">
-            Vol-
-          </button>
-        </li>
-      </ul>
+      <Grid container className={classes.controls} id="audio-controls">
+        <Grid item container xs={12} justify="center" alignItems="center">
+          <Grid item xs={2} className={classes.center}>
+            <Typography variant="caption">
+              {formatSeconds(currentTime)}
+            </Typography>
+          </Grid>
+          <Grid item xs={8}>
+            <Slider value={progress} onChange={changeProgress} />
+          </Grid>
+          <Grid item xs={2} className={classes.center}>
+            <Typography variant="caption">{formatSeconds(duration)}</Typography>
+          </Grid>
+        </Grid>
+        <Grid item container xs={12} justify="center" wrap="nowrap">
+          <Grid item xs={2} className={classes.center}>
+            <IconButton onClick={skipBackwards}>
+              <Replay10Icon fontSize="large" />
+            </IconButton>
+          </Grid>
+          <Grid item xs={2} className={classes.center}>
+            <IconButton>
+              <SkipPreviousIcon fontSize="large" />
+            </IconButton>
+          </Grid>
+          <Grid item xs={2} className={classes.center}>
+            <IconButton onClick={playPause}>
+              {!audio || audio.paused ? (
+                <PlayArrowIcon fontSize="large" />
+              ) : (
+                <PauseIcon fontSize="large" />
+              )}
+            </IconButton>
+          </Grid>
+          <Grid item xs={2} className={classes.center}>
+            <IconButton>
+              <SkipNextIcon fontSize="large" />
+            </IconButton>
+          </Grid>
+          <Grid item xs={2} className={classes.center}>
+            <IconButton onClick={skipForward}>
+              <Forward10Icon fontSize="large" />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Grid>
     </figure>
   );
 }
