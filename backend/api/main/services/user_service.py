29d656e95677f2c8e16e197a873af5a43f014204
johnp@eiqmobility.com <yjpan47@gmail.com>
2020-10-07 15:47:16 +0800

    updates

--- backend/api/main/services/user_service.py
+++ backend/api/main/services/user_service.py
@@ -1,63 +1,197 @@
-import uuid
+from auth0.v3 import Auth0Error
 from flask import jsonify, make_response
-from api.main.utils.auth_util import AuthService
+from pymongo.errors import OperationFailure, ConnectionFailure
+
+from api.main.models.user_model import UserModel
+from api.main.utils.auth_util import AuthUtil
 
 
 class UserService:
     @staticmethod
     def register_user(username, email, password):
-        user_id = uuid.uuid4().hex
-        user_data_resp, status_code = AuthService.create_user(
-            user_id=user_id,
-            username=username,
-            email=email,
-            password=password
-        )
-        if status_code == 201:
-            user_token_resp, status_code = AuthService.get_user_token(
-                username_or_email=username,
+        try:
+            user_auth_data = AuthUtil.create_user(
+                username=username,
+                email=email,
                 password=password
             )
-            if status_code == 200:
-                user_token = user_token_resp['access_token']
-                data = {
-                    'user_id': user_id,
-                    'username': user_data_resp['username'],
-                    'email': user_data_resp['email'],
-                    'user_token': user_token
-                }
-                return make_response(jsonify(data), status_code)
-            else:
-                return make_response(jsonify(user_token_resp), status_code)
-        else:
-            return make_response(jsonify(user_data_resp), status_code)
+            user_token = AuthUtil.get_user_token(
+                    username_or_email=username,
+                    password=password
+            )
+            user_app_data = UserModel.add_user(
+                username=username
+            )
+            data = {
+                'username': user_auth_data['username'],
+                'email': user_auth_data['email'],
+                'followers': user_app_data['followers'],
+                'followings': user_app_data['followings'],
+                'posts': user_app_data['posts'],
+                'user_token': user_token
+            }
+            return make_response(
+                jsonify({
+                    'status': 'success',
+                    'message': 'User successfully registered.',
+                    'data': data
+                }), 201
+            )
+        except Auth0Error as e:
+            return make_response(
+                jsonify({
+                    'status': 'fail',
+                    'message': f'Auth0 Error: {str(e)}',
+                }), 400
+            )
+        except OperationFailure as e:
+            return make_response(
+                jsonify({
+                    'status': 'fail',
+                    'message': f'DB Operation Error: {str(e)}',
+                }), 400
+            )
+        except ConnectionFailure as e:
+            return make_response(
+                jsonify({
+                    'status': 'fail',
+                    'message': f'DB Connection Error: {str(e)}',
+                }), 500
+            )
 
     @staticmethod
     def login_user(username_or_email, password):
-        user_token_resp, status_code = AuthService.get_user_token(
-            username_or_email=username_or_email,
-            password=password
-        )
-        if status_code == 200:
-            user_token = user_token_resp['access_token']
-            decode_resp = AuthService.decode_user_token(user_token)
-            if decode_resp['status'] == 'success':
-                user_id = decode_resp['user_id']
-                user_data_resp, status_code = AuthService.get_user(user_id)
-                if status_code == 200:
-                    data = {
-                        'user_id': user_id,
-                        'username': user_data_resp['username'],
-                        'email': user_data_resp['email'],
-                        'user_token': user_token
-                    }
-                    return make_response(jsonify(data), status_code)
-                else:
-                    return make_response(jsonify(user_data_resp), status_code)
-            else:
-                return make_response(jsonify(decode_resp), 400)
-        else:
-            return make_response(jsonify(user_token_resp), status_code)
+        try:
+            user_token = AuthUtil.get_user_token(
+                username_or_email=username_or_email,
+                password=password
+            )
+            username = AuthUtil.decode_user_token(user_token)
+            user_auth_data = AuthUtil.get_user(username)
+            user_app_data = UserModel.get_user(username)
+            data = {
+                'username': user_auth_data['username'],
+                'email': user_auth_data['email'],
+                'followers': user_app_data['followers'],
+                'followings': user_app_data['followings'],
+                'posts': user_app_data['posts'],
+                'user_token': user_token
+            }
+            return make_response(
+                jsonify({
+                    'status': 'success',
+                    'message': 'User successfully login.',
+                    'data': data
+                }), 200
+            )
+        except Auth0Error as e:
+            return make_response(
+                jsonify({
+                    'status': 'fail',
+                    'message': f'Auth0 Error: {str(e)}',
+                }), 400
+            )
+        except OperationFailure as e:
+            return make_response(
+                jsonify({
+                    'status': 'fail',
+                    'message': f'DB Operation Error: {str(e)}',
+                }), 400
+            )
+        except ConnectionFailure as e:
+            return make_response(
+                jsonify({
+                    'status': 'fail',
+                    'message': f'DB Connection Error: {str(e)}',
+                }), 500
+            )
+
+    @staticmethod
+    def get_user(username, auth_info=True):
+        try:
+            data = {'username': username}
+            user_app_data = UserModel.get_user(username)
+            data.update({
+                'username': username,
+                'followers': user_app_data['followers'],
+                'followings': user_app_data['followings'],
+                'posts': user_app_data['posts'],
+            })
+            if auth_info:
+                user_auth_data = AuthUtil.get_user(username)
+                data.update({
+                    'email': user_auth_data['email'],
+                })
+            return make_response(
+                jsonify({
+                    'status': 'success',
+                    'message': 'User successfully retrieved.',
+                    'data': data
+                }), 200
+            )
+        except Auth0Error as e:
+            return make_response(
+                jsonify({
+                    'status': 'fail',
+                    'message': f'Auth0 Error: {str(e)}',
+                }), 400
+            )
+        except OperationFailure as e:
+            return make_response(
+                jsonify({
+                    'status': 'fail',
+                    'message': f'DB Operation Error: {str(e)}',
+                }), 400
+            )
+        except ConnectionFailure as e:
+            return make_response(
+                jsonify({
+                    'status': 'fail',
+                    'message': f'DB Connection Error: {str(e)}',
+                }), 500
+            )
+
+    @staticmethod
+    def follow(username, other_username):
+        try:
+            user_app_data = UserModel.follow(username, other_username)[0]
+            user_auth_data = AuthUtil.get_user(username)
+            data = {
+                'username': user_auth_data['username'],
+                'email': user_auth_data['email'],
+                'followers': user_app_data['followers'],
+                'followings': user_app_data['followings'],
+                'posts': user_app_data['posts'],
+            }
+            return make_response(
+                jsonify({
+                    'status': 'success',
+                    'message': f'{username} successfully followed {other_username}.',
+                    'data': data
+                }), 200
+            )
+        except Auth0Error as e:
+            return make_response(
+                jsonify({
+                    'status': 'fail',
+                    'message': f'Auth0 Error: {str(e)}',
+                }), 400
+            )
+        except OperationFailure as e:
+            return make_response(
+                jsonify({
+                    'status': 'fail',
+                    'message': f'DB Operation Error: {str(e)}',
+                }), 400
+            )
+        except ConnectionFailure as e:
+            return make_response(
+                jsonify({
+                    'status': 'fail',
+                    'message': f'DB Connection Error: {str(e)}',
+                }), 500
+            )
+
 
     @staticmethod
     def logout():
--- backend/api/main/utils/auth_util.py
+++ backend/api/main/utils/auth_util.py
@@ -1,13 +1,14 @@
 import os
 import jwt
 import requests
+from auth0.v3 import Auth0Error
 from flask_httpauth import HTTPTokenAuth
 from cryptography.x509 import load_pem_x509_certificate
 from cryptography.hazmat.backends import default_backend
 from api.main.config import AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_CERT, LOGGER
 
 
-class AuthService:
+class AuthUtil:
     @staticmethod
     def get_auth_token():
         url = os.path.join(AUTH0_DOMAIN, 'oauth/token/')
@@ -18,8 +19,15 @@ class AuthService:
             "grant_type": "client_credentials"
         }
         resp = requests.post(url, data=payload)
-        token = resp.json().get('access_token')
-        return token
+        resp_data = resp.json()
+        if resp.status_code == 200:
+            token = resp_data.get('access_token')
+            return token
+        raise Auth0Error(
+            status_code=resp.status_code,
+            error_code=resp_data.get('errorCode'),
+            message=f'Error in retrieving application access token. {resp_data.get("message", "")}'
+        )
 
     @staticmethod
     def get_user_token(username_or_email, password):
@@ -34,60 +42,76 @@ class AuthService:
             "connection": "Username-Password-Authentication",
         }
         resp = requests.post(url, data=payload)
-        return resp.json(), resp.status_code
+        resp_data = resp.json()
+        if resp.status_code == 200:
+            token = resp_data['access_token']
+            return token
+        raise Auth0Error(
+            status_code=resp.status_code,
+            error_code=resp_data.get('errorCode'),
+            message=f'Error in retrieving user access token. {resp_data}'
+        )
 
     @staticmethod
     def decode_user_token(user_token):
-        try:
-            cert_obj = load_pem_x509_certificate(str.encode(AUTH0_CERT), default_backend())
-            payload = jwt.decode(
-                user_token,
-                key=cert_obj.public_key(),
-                algorithms="RS256",
-                options={'verify_aud': False}
-            )
-            return {
-                'status': 'success',
-                'message': 'Valid token.',
-                'user_id': payload['sub'].split('|')[1]
-            }
-        except jwt.ExpiredSignatureError:
-            return {
-                'status': 'fail',
-                'message': 'Signature expired.'
-            }
-        except jwt.InvalidTokenError:
-            return {
-                'status': 'fail',
-                'message': 'Invalid token.'
-            }
+        cert_obj = load_pem_x509_certificate(str.encode(AUTH0_CERT), default_backend())
+        payload = jwt.decode(
+            user_token,
+            key=cert_obj.public_key(),
+            algorithms="RS256",
+            options={'verify_aud': False}
+        )
+        sub = payload['sub'].split('|')[1]
+        return sub
 
     @staticmethod
-    def create_user(user_id, username, password, email):
-        auth_token = AuthService.get_auth_token()
+    def create_user(username, password, email):
+        auth_token = AuthUtil.get_auth_token()
         url = os.path.join(AUTH0_DOMAIN, 'api/v2/users')
         headers = {
-            "Authorization": "Bearer " + auth_token
+            'Authorization': 'Bearer ' + auth_token
         }
         payload = {
-            'user_id': user_id,
+            'user_id': username,
             'username': username,
             'password': password,
             'email': email,
             "connection": "Username-Password-Authentication",
         }
         resp = requests.post(url, headers=headers, data=payload)
-        return resp.json(), resp.status_code
+        resp_data = resp.json()
+        if resp.status_code == 201:
+            user_info = {
+                'username': username,
+                'email': email,
+            }
+            return user_info
+        raise Auth0Error(
+            status_code=resp.status_code,
+            error_code=resp_data.get('errorCode'),
+            message=f'Error creating user. {resp_data}'
+        )
 
     @staticmethod
-    def get_user(user_id):
-        auth_token = AuthService.get_auth_token()
-        url = os.path.join(AUTH0_DOMAIN, f'api/v2/users/auth0|{user_id}')
+    def get_user(username):
+        auth_token = AuthUtil.get_auth_token()
+        url = os.path.join(AUTH0_DOMAIN, f'api/v2/users/auth0|{username}')
         headers = {
             "Authorization": "Bearer " + auth_token
         }
         resp = requests.get(url, headers=headers)
-        return resp.json(), resp.status_code
+        resp_data = resp.json()
+        if resp.status_code == 200:
+            user_info = {
+                'username': username,
+                'email': resp_data['email']
+            }
+            return user_info
+        raise Auth0Error(
+            status_code=resp.status_code,
+            error_code=resp_data.get('errorCode'),
+            message=f'Error retrieving user. {resp_data}'
+        )
 
 
 TOKEN_AUTH = HTTPTokenAuth()
@@ -95,8 +119,8 @@ TOKEN_AUTH = HTTPTokenAuth()
 
 @TOKEN_AUTH.verify_token
 def verify_token(auth_token):
-    response = AuthService.decode_user_token(auth_token)
-    LOGGER.info(response)
-    if response['status'] == 'success':
-        data = response['data']
-        return data.get('sub').split('|')[1]
+    try:
+        username = AuthUtil.decode_user_token(auth_token)
+        return username
+    except:
+        return None
