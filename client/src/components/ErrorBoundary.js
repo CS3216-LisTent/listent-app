64824738ca14ea113b7885067e7476b968ef7745
nelsontky <nelson@ccb.wtf>
2020-10-08 00:41:36 +0800

    Get profile from backend

--- client/src/components/ErrorBoundary.js
+++ client/src/components/ErrorBoundary.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 
 export default class ErrorBoundary extends React.Component {
   state = { hasError: false, error: null };
@@ -11,10 +12,14 @@ export default class ErrorBoundary extends React.Component {
   }
 
   render() {
-    if (this.state.hasError) {
-      return this.props.fallback;
+    if (!this.state.hasError) {
+      return this.props.children;
     }
 
-    return this.props.children;
+    if (this.state.error.message.includes("status code 404")) {
+      return <Redirect to="/" />;
+    } else {
+      return this.props.fallback;
+    }
   }
 }
