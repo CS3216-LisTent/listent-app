bad26448abb13ef8d79f2780d968b13b3e5175fc
nelsontky <nelson@ccb.wtf>
2020-10-24 02:18:22 +0800

    Add hash tag support

--- client/src/components/DetectLinks.js
+++ client/src/components/DetectLinks.js
@@ -1,28 +1,18 @@
 import React from "react";
 import Linkify from "react-linkify";
-import clsx from "clsx";
-import { makeStyles } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
-  linkColor: { color: theme.palette.primary.main },
-}));
+export default function DetectLinks({ children, isOpenSameTab }) {
+  const theme = useTheme();
 
-export default function DetectLinks({ className, children, isOpenSameTab }) {
-  const classes = useStyles();
-
-  const componentDecorator = (href, text, key) => {
-    return (
-      <a
-        className={clsx(classes.linkColor, className)}
-        href={href}
-        key={key}
-        target={isOpenSameTab ? undefined : "_blank"}
-        rel={isOpenSameTab ? undefined : "noopener noreferrer"}
-      >
-        {text}
-      </a>
-    );
-  };
-
-  return <Linkify componentDecorator={componentDecorator}>{children}</Linkify>;
+  return (
+    <Linkify
+      properties={{
+        style: { color: theme.palette.primary.main },
+        target: isOpenSameTab ? undefined : "_blank",
+      }}
+    >
+      {children}
+    </Linkify>
+  );
 }
