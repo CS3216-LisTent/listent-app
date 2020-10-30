233adcfc06d21d138bde435de50f1c8531aa115b
nelsontky <nelson@ccb.wtf>
2020-10-31 03:27:38 +0800

    Fix security

---
+++ client/src/utils/convert-to-chipmunk-2.js
@@ -0,0 +1,11 @@
+import bufferToWave from "audiobuffer-to-wav";
+
+const audioContext = new (window.AudioContext || window.webkitAudioContext)()
+
+
+export default function convertToChipmunk(
+  audioBlob,
+  setAudioSrc,
+  setRecordedBlob,
+  setConversionProgress
+) {}
